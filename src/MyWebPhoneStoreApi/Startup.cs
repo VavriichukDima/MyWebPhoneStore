b286aa8771b3b8a235e020085e064ab2f021724f
Dima Vavriychuk <tiponchik@gmail.com>
2021-07-08 21:44:16 +0300

    Created empty projects

--- src/MyWebPhoneStoreApi/Startup.cs
+++ src/MyWebPhoneStoreApi/Startup.cs
@@ -1,22 +1,21 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace MyWebPhoneStore
+namespace MyWebPhoneStoreApi
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
@@ -26,10 +25,11 @@ namespace MyWebPhoneStore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MyWebPhoneStore", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MyWebPhoneStoreApi", Version = "v1" });
             });
         }
 
@@ -40,10 +40,8 @@ namespace MyWebPhoneStore
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MyWebPhoneStore v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MyWebPhoneStoreApi v1"));
             }
-
-            app.UseHttpsRedirection();
 
             app.UseRouting();
 
